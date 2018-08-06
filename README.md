63277a28a769d576323adfac41d1df5c0389092f
ChewieB <chewiebaker@gmail.com>
2018-08-06 19:38:07 +0100

    Serial port ID corrected

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Read Serial GPS NEMA returs a Lat Lon pair in a string.
 
 CLI params select between (Lon Lat), Alt, (Speed heading), status data and a free running input directly sent to the output.
+
+To Build
+gcc -Wall main.c -o gps
--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <termios.h>/* POSIX Terminal Control Definitions*/
 #include <errno.h>  /* ERROR Number Definitions          */
 #include <unistd.h> /* UNIX Standard Definitions         */
-void main()
+char* main()
 {
-  int fd;
-  fd = open("/dev/ttyUSB0",O_RDWR | O_NOCTTY);
-  if(fd == ­1)
-     printf("\n  Error! in Opening ttyUSB0\n");
-  else
-     printf("\n  ttyUSB0 Opened Successfully\n");
-  close(fd);
+	int fd;
+	fd = open("/dev/ttyAMA0",O_RDWR | O_NOCTTY);
+	if(fd == 1)
+	{
+		printf("\n  Error! in Opening ttyAMA0\n");
+	}
+	else
+		printf("\n  ttyAMA0 Opened Successfully\n");
+	close(fd);
+	return "Hello";
 }
 
