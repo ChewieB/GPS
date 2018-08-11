500fd85330b2b5b9ac28d3f060577d5b07e47d29
ChewieB <chewiebaker@gmail.com>
2018-08-11 22:34:07 +0100

    filtered serial data for strings starting

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <termios.h>/* POSIX Terminal Control Definitions*/
 #include <errno.h>  /* ERROR Number Definitions          */
 #include <unistd.h> /* UNIX Standard Definitions         */
-//char* main()
-void main()
+char* main()
+//void main()
 {
 	int fd;
 	fd = open("/dev/ttyAMA0",O_RDWR | O_NOCTTY);
@@ -39,31 +39,34 @@ void main()
         SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // No$
 
         SerialPortSettings.c_oflag &= ~OPOST;//No Output Processing
-        // Setting Time outs 
-        SerialPortSettings.c_cc[VMIN] = 100; // Read at least 10 characters
-        SerialPortSettings.c_cc[VTIME] = 0; // Wait indefinetly   
+        // Setting Time outs
+        SerialPortSettings.c_cc[VMIN] = 150; // Read at least 150 characters
+        SerialPortSettings.c_cc[VTIME] = 0;  // Wait indefinetly
 
-        if((tcsetattr(fd, TCSANOW, &SerialPortSettings)) != 0) // Set the attri$
-        	printf("\n  ERROR ! in Setting attributes");
-        else
-		printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+        if((tcsetattr(fd, TCSANOW, &SerialPortSettings)) != 0) // Set the attri
+	{
+        	printf("  ERROR ! in Setting attributes\n");
+		return "Error setting attributes";
+	}
 
         //------------------------------- Read data from serial port --------
 
-        tcflush(fd, TCIFLUSH);   // Discards old data in the rx buffer       
+        tcflush(fd, TCIFLUSH);  // Discards old data in the rx buffer
 
-        char read_buffer[32];   // Buffer to store the data received         
-        int  bytes_read = 0;    // Number of bytes read by the read() system 
-        int i = 0;
+        char read_buffer[150];  // Buffer to store the data received
+        int  bytes_read = 0;	// Number of bytes read by the read() system
 
-        bytes_read = read(fd,&read_buffer,32); // Read the data
-
-        printf("\n\n  Bytes Rxed -%d", bytes_read); // Print the number of by
-        printf("\n\n  ");
-
-	for(i=0;i<bytes_read;i++)        //printing only the received charact
-	        printf("%c",read_buffer[i]);
+	while(true)
+	{
+	        bytes_read = read(fd,&read_buffer,140); // Read the data
+	        printf("  Bytes Rxed [%i]\n", bytes_read); // Print the number of bytes
+		if (read_buffer[0] == '$' && read_buffer[1] == 'G')
+		{
+			printf("%s - break\n",read_buffer);
+			break;
+		}
+	}
 
 	close(fd);
-//	return "Hello";
+	return read_buffer;
 }
