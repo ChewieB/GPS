0841c11fb96280e5efa822cc78cdfde2cbc3d1d8
ChewieB <chewiebaker@gmail.com>
2018-08-14 22:01:29 +0100

    Working Code :)

--- main.c
+++ main.c
@@ -15,13 +15,8 @@ int OpenFile(void)
 	{
 		printf("Error! in Opening ttyAMA0\n");
 		printf("fd = [%d]\n",fd);
+		return -1;
 	}
-	else
-	{
-		printf("ttyAMA0 Opened Successfully\n");
-		printf("fd = [%i]\n",fd);
-	}
-
 	return fd;
 }
 
@@ -98,41 +93,41 @@ int main()
 
 	char* p;
 
-	printf("data = [%s]\n", read_buffer);
+//	printf("data = [%s]\n", read_buffer);
 	int comma2 = 0;
 	int comma3 = 0;
 	p = strchr(read_buffer,',');
-	printf("first comma at [%d]\n",p - read_buffer);
+//	printf("first comma at [%d]\n",p - read_buffer);
 	p = strchr(p + 1, ',');
 	comma2 = p - read_buffer;
-	printf("second comma at [%d]\n", comma2);
+//	printf("second comma at [%d]\n", comma2);
 	p = strchr(p + 1, ',');
 	comma3 = p - read_buffer;
-	printf("third comma at [%d]\n", comma3);
+//	printf("third comma at [%d]\n", comma3);
 
 	char sub[1000];
 	int c = 0, length, position;
 	length = comma3 - comma2;
 	position = comma2;
-	printf("position = %d length = %d\n", position, length);
+//	printf("position = %d length = %d\n", position, length);
 	while (c < length - 1)
 	{
 		sub[c] = read_buffer[position + 1 + c];
       		c++;
    	}
    	sub[c] = '\0';
-	printf("Northing = [%s]\n", sub);
+//	printf("Northing = [%s]\n", sub);
 
 	float fred = atof(sub)/100.0F;
-	printf("Number fred = [%f]\n", fred);
+//	printf("Number fred = [%f]\n", fred);
 
 	p = strchr(p + 1, ',');
 	int comma4 = p - read_buffer;
 	p = strchr(p + 1, ',');
 	int comma5 = p - read_buffer;
 	char ns = read_buffer[comma3 + 1];
-	printf("forth comma at [%d]\n", comma4);
-	printf("Lat hemi = [%c]\n", ns);
+//	printf("forth comma at [%d]\n", comma4);
+//	printf("Lat hemi = [%c]\n", ns);
 
 	length = comma5 - comma4;
 	position = comma4 + 1;
@@ -141,10 +136,13 @@ int main()
 		sub[i] = read_buffer[position + i];
 	}
 	sub[c] = '\0';
-	printf("Easting = [%s]\n", sub);
+//	printf("Easting = [%s]\n", sub);
 	float East = atof(sub)/100.0F;
 
-	printf("****Result = %f %c %f ****\n", fred, ns, East);
+	char ew = read_buffer[comma5 + 1];
+
+	printf("%f %c %f %c", fred, ns, East, ew);
+
 	close(fd);
 	return 1;
 }
